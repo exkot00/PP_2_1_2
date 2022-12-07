eba22c58918e1c860115b6037b28ca2774193d0b
exkot00 <114568769+exkot00@users.noreply.github.com>
2022-12-07 13:38:31 +0500

    c1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == bean1);
+     //   System.out.println(bean.getMessage().equals(bean1.getMessage()));
+
+        System.out.println(beanCat == beanCat1);
+     //   System.out.println(beanCat.getMessage().equals(bean1.getMessage()));
+
+
     }
 }
